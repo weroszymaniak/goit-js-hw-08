2dd5b22b8fc72a0c94af30a4566cfb754d1139be
weroszymaniak <weronika.szymaniak@hotmail.com>
2023-03-31 18:57:44 +0200

    changes

---
+++ src/js/01-gallery.js
@@ -0,0 +1,26 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+// Change code below this line
+
+console.log(galleryItems);
+
+const gallerySet = document.querySelector('.gallery');
+
+const galleryProject = galleryItems
+  .map(
+    image =>
+      `<a class="gallery__item" href="${image.original}"> <img class = "gallery__image" src = "${image.preview}" 
+      alt = "${image.description}" data-source = "${image.original}" /> </a> </div>`
+  )
+  .join('');
+
+gallerySet.insertAdjacentHTML('afterbegin', galleryProject);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionPosition: 'bottom',
+});
