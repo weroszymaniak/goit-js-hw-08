61048eef7dbe16ba15775d9c540743581f9994ff
weroszymaniak <weronika.szymaniak@hotmail.com>
2023-04-01 13:33:45 +0200

    small update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,7 +5,7 @@ const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 const LOCALSTORAGE_KEY = 'videoplayer-current-time';
 
-player.setCurrentTime(local.Storage.getItem(LOCALSTORAGE_KEY) || 0);
+player.setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY) || 0);
 
 const setTime = time => localStorage.setItem(LOCALSTORAGE_KEY, time.seconds);
 player.on('timeupdate', throttle(setTime, 1000));
